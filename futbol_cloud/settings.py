0259b591417fe7bf8f9bbeb05e9c6b9ef1907ee1
Franko1710 <Frankocartagena@gmail.com>
2025-05-16 15:24:43 -0400

    se activo el host

--- futbol_cloud/settings.py
+++ futbol_cloud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qsgdqgs(7e+&w)x+i9*(^ub4p-!q7n1vg+ci!_6=ylfwn*6%6w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
